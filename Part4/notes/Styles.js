22b5558a667415cd1e93349239199f7613321aa7
Mirva <mirva.tapola@gmail.com>
2020-04-30 20:29:26 +0300

    viimeistely

--- Part4/notes/Styles.js
+++ Part4/notes/Styles.js
@@ -1,42 +1,56 @@
 import { StyleSheet } from "react-native";
 
-// flexDirection, alignItems, and justifyContent to change the layout.
 const styles = StyleSheet.create({
     note: {
-        fontSize: 25,
-        padding: 30
+        fontSize: 20,
+        padding: 25,
+        color: '#242423'
     },
     header: {
         flex:1,
-        backgroundColor: '#119DA4',
-        fontSize: 30,
+        backgroundColor: '#066875',
+        fontSize: 25,
         fontWeight: 'bold',
         textAlign: 'center',
-        padding: 15,
-        color: '#040404',
-        //justifyContent: 'flex-end'
+        padding: 10,
+        color: '#ECECEC',
     },
     input: {
         fontSize: 20,
-        padding: 10,
+        padding: 15,
         backgroundColor: '#E7E8DE'
     },
-    footer: {
+    notesection: {
+        flex: 2,
+        flexGrow: 3,
+    },
+    bottom: {
         flex: 1,
         marginBottom: 36
     },
     container: {
         flex: 1,
-        backgroundColor: '#DFE1D6',
-        //justifyContent: 'flex-end',
+        backgroundColor: '#D9D9D9',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
     },
     button: {
-        backgroundColor: '#0C7489',
-        color: 'white',
+        backgroundColor: '#066875',
+        color: '#E1E1E1',
         fontSize: 20,
         padding: 20,
-        //alignItems: 'center',
-        justifyContent: 'center'
+        textAlign: 'center',
+        fontWeight: 'bold',
+        borderRadius: 50,
+        margin: 10
+    },
+    delete:{
+        backgroundColor: '#E67B86',
+        padding: 15,
+        fontSize: 15,
+        textAlign: 'center',
+        borderRadius: 50,
+        marginHorizontal: 10,
     }
 })
 
